1f67da6f453a63ec13ba8e0e8e52c2aa5bcd5cfe
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-12-21 09:34:08 -0300

    list with filter and pagination

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('', require('../services/customers/create'));
+router.get('', require('../services/customers/list'));
 router.get('/:id', require('../services/customers/listOne'));
 router.put('/:id', require('../services/customers/update'));
 router.put('/:id/delete', require('../services/customers/delete'));
---
+++ src/services/customers/list.js
@@ -0,0 +1,60 @@
+const Customers = require('../../schemas/customers');
+const Phones = require('../../schemas/phones');
+const { Op } = require('sequelize');
+
+module.exports = async (req, res) => {
+    let params = [];
+    let paramsPhones = [];
+
+    let size = req.query.size ? parseInt(req.query.size) : 10;
+    let page = req.query.page ? parseInt(req.query.page) : 1;
+
+    if (req.query.isActive == 'false')
+        params.push({ isActive: 0 })
+    else
+        params.push({ isActive: 1 })
+    
+    if (req.query.name) 
+        params.push({ name: { [Op.like]: `%${req.query.name}%`} })
+
+    if (req.query.phoneNumber)
+        paramsPhones.push({ number: { [Op.like]: `%${req.query.phoneNumber}%`} })
+
+    const customers = await Customers.findAndCountAll({
+        offset: (page - 1) * size,
+        limit: size,
+        order: [
+            ['name', 'ASC']
+        ],
+        include: [{
+            model: Phones,
+            as: 'phones',
+            attributes: ['id', 'ddd', 'number', 'customerId'],
+            required: paramsPhones.length > 0 ? true : false,
+            where: {
+                [Op.and]: paramsPhones
+            }
+        }],
+        where: {
+            [Op.and]: params
+        }
+    })
+
+    if (customers.rows.length == 0)
+        return res.status(404).json({ message: 'not found customers' });
+
+    return res.status(200).json({
+        "content": customers.rows,
+        "pageable": {
+            "pageNumber": page,
+            "pageSize": size
+        },
+        "totalPages": Math.ceil(customers.rows.length / size),
+        "totalElements": customers.rows.length,
+        "last": Math.ceil(customers.count / size) == page ? true : false,
+        "size": size,
+        "first": page == 1 ? true : false,
+        "numberOfElements": customers.rows.length,
+        "empty": customers.rows == 0 ? true : false
+    });
+}
